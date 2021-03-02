3d4d74bbf7225f3e16891be005c8486b30d3c8d9
subbotin <subbotin@haulmont.com>
2021-03-02 15:27:24 +0400

    Fix spotbugs for security module (port to 0.3) Haulmont/jmix-security#62

--- security-data/src/main/java/io/jmix/securitydata/user/AbstractDatabaseUserRepository.java
+++ security-data/src/main/java/io/jmix/securitydata/user/AbstractDatabaseUserRepository.java
@@ -151,7 +151,7 @@ public abstract class AbstractDatabaseUserRepository<T extends UserDetails> impl
     }
 
     @Override
-    public void changePassword(@Nullable String userName, @Nullable String oldPassword, @Nullable String newPassword) throws PasswordNotMatchException {
+    public void changePassword(String userName, @Nullable String oldPassword, @Nullable String newPassword) throws PasswordNotMatchException {
         Preconditions.checkNotNullArgument(userName, "Null userName");
         Preconditions.checkNotNullArgument(newPassword, "Null new password hash");
         T userDetails = loadUserByUsername(userName);
